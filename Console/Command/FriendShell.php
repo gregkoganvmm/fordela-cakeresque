0317595ba19a63a0435f5d877bee28b4a622a006
Zack Small <zack@fordela.com>
2015-05-26 13:46:54 -0700

    saving snippets

--- Console/Command/FriendShell.php
+++ Console/Command/FriendShell.php
@@ -8,12 +8,41 @@ class FriendShell extends AppShell
     public $status = null;
     public $retry = 3;
 
-    // TODO: Should perform() be removed from all shells?
-    // Doesn't look like it does anything and all works when it's commented
-    /*public function perform() {
-        $this->initialize();
-        $this->{array_shift($this->args)}();
+    var $uses = array('Video');
+
+    /*public function test()
+    {
+        $s3 = Aws::factory(AWS_CONFIG)->get('S3');
+        $s3->registerStreamWrapper();
+        
+        $video_file = TMP.'downloads/995/videos/test1.mp4';
+        //$video_file = TMP.'downloads/995/videos/test1.mov';
+
+        $result = $s3->getObject(array(
+            'Bucket' => 'media.fordela.com',
+
+            // TODO: Seems filesize changes where it's found
+
+            'Key'    => '2672/Hollywood_Babylon_Soft_Sample.mp4', // About 70MB
+            //'Range'  => 'bytes=0-20000', // mp4
+            'Range'  => 'bytes=0-2000000', // mov - 20MB or less 100000 works
+            'SaveAs' => $video_file
+        ));
+        $mediainfo = $this->Video->mediainfo($video_file);
+        debug($mediainfo);
     }*/
+
+    public function rename()
+    {
+        $file1 = 'this is my file.mp4';
+        $file_path = TMP.'downloads/995/videos/';
+        $newfile = $file_path.'this_is_my_file.mp4';
+
+        $this->out('Renaming file: '.$file1.' to '.$newfile);
+
+        rename($file_path.$file1,$newfile);
+        $this->out('Finished');
+    }
 
     public function doSomething() 
     {
