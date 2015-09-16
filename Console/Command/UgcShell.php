cff6b5f993a8dceb34559ce135f6df194907e260
zasmall <zack@fordela.com>
2015-09-16 12:04:42 -0700

    Adding function for using ffmpeg to create thumbnail then ftp it to 3DVL

--- Console/Command/UgcShell.php
+++ Console/Command/UgcShell.php
@@ -53,6 +53,10 @@ class UgcShell extends Shell {
 
         $info = pathinfo($version);
 
+        //TODO: Create thumbnail
+        $this->thumbnail($info['filename']);
+
+
         //TODO: On Successful upload notify 3DVL & cleanup
         //The following is copied from the encoder git repo.  Need to modify slightly for our purposes here.
         //Drupal expects: reference, status, source
@@ -76,6 +80,47 @@ class UgcShell extends Shell {
         $this->status['finished'] = date('Y-m-d H:i:s');
         $jobId = end($this->args);
         $this->JobQueue->updateJob($jobId,$this->status);
+    }
+
+    protected function thumbnail($node_id)
+    {
+
+        $video_file = TMP.'uploads/ugc/version/'.$node_id.'.mp4';
+        $image = $node_id.'_thumb.jpg'; //PNG???
+        $thumb_path = TMP.'uploads/ugc/version/';
+
+        exec("ffmpeg -i " . $video_file  . " -ss 00:00:10 -r 1 -an -vframes 1 -f mjpeg " . $thumb_path.$image);
+        if(!file_exists($thumb_path.$image)) {
+            // Try a second attempt if it failed
+            exec("ffmpeg -i " . $video_file  . " -ss 00:00:05 -r 1 -an -vframes 1 -f mjpeg " . $thumb_path.$image);
+        }
+
+        //TODO: FTP thumbnail to 3DVL
+        // $thumb_destination = "ftp://drupal:yeahbaby@3dvisionlive.fordela.com/sites/default/files/ugc/images/".$image;
+
+        // connect and login to FTP server
+        $ftp_server = "dvisionlive.fordela.com";
+        $ftp_username = 'drupal';
+        $ftp_userpass = 'yeahbaby';
+
+        $ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
+        $login = ftp_login($ftp_conn, $ftp_username, $ftp_userpass);
+        ftp_pasv($ftp_conn, true);
+        ftp_chdir($ftp_conn, "sites/default/files/ugc/images");
+
+        $file = $thumb_path.$image;
+
+        // upload version
+        if (ftp_put($ftp_conn, $image, $file, FTP_BINARY)) { // was FTP_ASCII
+            echo "Successfully uploaded $file.";
+        } else {
+            echo "Error uploading $file.";
+        }
+
+        // close connection
+        ftp_close($ftp_conn);
+        
+        return true;
     }
 
     public function processUgc()
